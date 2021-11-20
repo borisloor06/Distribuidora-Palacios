e1fb67e812103479a4021ec2327b06d0f29900c1
Boris Loor <49875241+wolf12a@users.noreply.github.com>
2021-11-19 22:41:07 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+const headerMenu=document.querySelector('.hm-header');
+
+console.log(headerMenu.offsetTop);
+
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset > 80){
+        headerMenu.classList.add('header-fixed');
+    }else{
+        headerMenu.classList.remove('header-fixed');
+    }
+})
+
+/*=========================================
+    MENU
+==========================================*/
+
+const menu=document.querySelector('.icon-menu');
+const menuClose=document.querySelector('.cerrar-menu');
+
+menu.addEventListener('click',()=>{
+    document.querySelector('.header-menu-movil').classList.add('active');
+})
+
+menuClose.addEventListener('click',()=>{
+    document.querySelector('.header-menu-movil').classList.remove('active');
+})
